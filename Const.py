fd10810123438c7db38dacfa0b2a8a7cbdb8f5e3
lia gersht <liagersht1@gmail.com>
2022-10-25 12:47:29 +0300

    consts

--- Const.py
+++ Const.py
@@ -5,3 +5,42 @@ TITLE = 'ZooMaya Meetings Online - Video Conference - Chat Easily ! '
 # Images
 START_IMAGE = 'start_image.png'
 LOGO_IMAGE = 'ZooMaya_logo.png'
+# DOOR_WIDTH = 80
+# CORRIDOR_WIDTH = 130
+# FITTING_ROOM_WIDTH = 130
+# FITTING_ROOM_LENGTH = 170
+B_OWNER_QUESTION_CINEMA1 = "in case you have more than 1 floor,does your business have an elevator?(if you have 1 floor press 'yes')"
+B_OWNER_QUESTION_CINEMA2 = "does your door width equal or bigger than 80 cm?"
+B_OWNER_QUESTION_CINEMA3 = "do you have handicaps parking near your business?"
+B_OWNER_QUESTION_CINEMA4 = "does your door's business is transparent?"
+B_OWNER_QUESTION_CINEMA5 = "do you have special seats for handicaps?"
+B_OWNER_QUESTION_CINEMA6 = "in case you have stairs in the entrance,do you have a ramp?(if you don't have stairs press 'yes')"
+B_OWNER_QUESTION_CINEMA7 = "do you have a special sound system for hearing impaired?"
+B_OWNER_QUESTION_CINEMA8 = "do you have bathroom stalls for handicaps?"
+B_OWNER_QUESTION_CINEMA9 = "does your business's corridor width equal or bigger than 130 cm?"
+B_OWNER_QUESTION_CINEMA10 = "in case your door is transparent, do you have a colored sticker?(if your door isn't transparent press 'yes')"
+
+
+
+B_OWNER_QUESTION_RESTAURANT1 = "in case you have more than 1 floor,does your business have an elevator?(if you have 1 floor press 'yes')"
+B_OWNER_QUESTION_RESTAURANT2 = "does your door width equal or bigger than 80 cm?"
+B_OWNER_QUESTION_RESTAURANT3 = "does your door's business is transparent?"
+B_OWNER_QUESTION_RESTAURANT4 = "in case your door is transparent, do you have a colored sticker?(if your door isn't transparent press 'yes')"
+B_OWNER_QUESTION_RESTAURANT5 = "in case you have stairs in the entrance,do you have a ramp?(if you don't have stairs press 'yes')"
+B_OWNER_QUESTION_RESTAURANT6 = "do you have bathroom stalls for handicaps?"
+B_OWNER_QUESTION_RESTAURANT7 = "do you have handicaps parking near your business?"
+B_OWNER_QUESTION_RESTAURANT8 = "does your business's corridor width equal or bigger than 130 cm?"
+B_OWNER_QUESTION_RESTAURANT9 = "do you have a menu in Braille?"
+
+
+
+B_OWNER_QUESTION_STORE1 ="in case you have more than 1 floor,does your business have an elevator?(if you have 1 floor press 'yes')"
+B_OWNER_QUESTION_STORE2 ="does your door width equal or bigger than 80 cm?"
+B_OWNER_QUESTION_STORE3 = "does your door's business is transparent?"
+B_OWNER_QUESTION_STORE4 = "in case your door is transparent, do you have a colored sticker?(if your door isn't transparent press 'yes')"
+B_OWNER_QUESTION_STORE5 = "in case you have stairs in the entrance,do you have a ramp?(if you don't have stairs press 'yes')"
+B_OWNER_QUESTION_STORE6 = "do you have bathroom stalls for handicaps?"
+B_OWNER_QUESTION_STORE7 = "do you have handicaps parking near your business?"
+B_OWNER_QUESTION_STORE8 = "does your business's corridor width equal or bigger than 130 cm?"
+
+WHITE = (255, 255, 255)

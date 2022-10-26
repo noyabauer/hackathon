79047df2ab056515971690c838bea8720071ab28
noyabauer <noyab123@gmail.com>
2022-10-26 09:50:22 +0300

    reformat files

--- Database.py
+++ Database.py
@@ -1,44 +1,59 @@
 import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import credentials
-cred = credentials.Certificate('/Users/noyabauer/Desktop/mechina/hackathon/hackathon/hackathon-f082d-firebase-adminsdk-8xvjk-176383c3f5.json')
+
+cred = credentials.Certificate(
+    '/Users/noyabauer/Desktop/mechina/hackathon/hackathon/hackathon-f082d-firebase-adminsdk-8xvjk-176383c3f5.json')
 
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-def add_to_database(name,category,area,address,wheelchair,vision,hearing):
+
+def add_to_database(name, category, area, address, wheelchair, vision, hearing, website):
     data = {
         u'name': name,
         u'category': category,
         u'area': area,
         u'address': address,
         u'wheelchair': wheelchair,
-        u'vision':vision,
-        u'hearing':hearing
+        u'vision': vision,
+        u'hearing': hearing,
+        u'website': website
     }
 
     db.collection(u'Business-owners').document(name).set(data)
+
+
 def get_center_businesses():
     docs = db.collection(u'Business-owners').where(u'area', u'==', 'center').stream()
 
     for doc in docs:
         print(f'{doc.id} => {doc.to_dict()}')
+
+
 def get_north_businesses():
     docs = db.collection(u'Business-owners').where(u'area', u'==', 'north').stream()
     for doc in docs:
         print(f'{doc.id} => {doc.to_dict()}')
+
+
 def get_south_businesses():
     docs = db.collection(u'Business-owners').where(u'area', u'==', 'south').stream()
     for doc in docs:
         print(f'{doc.id} => {doc.to_dict()}')
+
+
 def get_wheelchair_businesses():
-    docs = db.collection(u'Business-owners').where(u'wheelchair', u'==',True ).stream()
+    docs = db.collection(u'Business-owners').where(u'wheelchair', u'==', True).stream()
     for doc in docs:
         print(f'{doc.id} => {doc.to_dict()}')
+
+
 def get_vision_businesses():
     docs = db.collection(u'Business-owners').where(u'vision', u'==', True).stream()
     for doc in docs:
         print(f'{doc.id} => {doc.to_dict()}')
+
 
 def get_hearing_businesses():
     docs = db.collection(u'Business-owners').where(u'hearing', u'==', True).stream()
@@ -46,14 +61,22 @@ def get_hearing_businesses():
         print(f'{doc.id} => {doc.to_dict()}')
 
 
-name= input("enter name ")
-category = input("enter category ")
-area = input("enter area ")
-address = input ("enter address ")
-wheelchair=True
-vision=False
-hearing= False
-# add_to_database(name,category,area,address,wheelchair,vision,hearing)
-get_center_businesses()
-get_north_businesses()
+def get_website(name):
+    cities_ref = db.collection(u'cities')
+    cities_ref.order_by(u'state').order_by(
+        u'population', direction=firestore.Query.DESCENDING)
+
+
+name = input("enter name ")
+# category = input("enter category ")
+# area = input("enter area ")
+# address = input("enter address ")
+# wheelchair = True
+# vision = False
+# hearing = False
+# website = input("enter a link for your website")
+# add_to_database(name, category, area, address, wheelchair, vision, hearing, website)
+# get_center_businesses()
+# get_north_businesses()
 # get_hearing_businesses()
+print (get_website(name))

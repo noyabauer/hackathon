11a052447ac3dd3976d9d61989bd7c88f8582b2c
mayador <mayador22@gmail.com>
2022-10-26 09:47:18 +0300

    change in gui

--- Const.py
+++ Const.py
@@ -9,12 +9,12 @@ LOGO_IMAGE = 'ZooMaya_logo.png'
 # CORRIDOR_WIDTH = 130
 # FITTING_ROOM_WIDTH = 130
 # FITTING_ROOM_LENGTH = 170
-B_OWNER_QUESTION_CINEMA1_WHEELCHAIR = "in case you have more than 1 floor,does your business have an elevator?(if you have 1 floor press 'yes')"
+B_OWNER_QUESTION_CINEMA1_WHEELCHAIR = "in case you have more than 1 floor,does your business have an elevator?\n(if you have 1 floor press 'yes')"
 B_OWNER_QUESTION_CINEMA2_WHEELCHAIR = "does your door width equal or bigger than 80 cm?"
 B_OWNER_QUESTION_CINEMA3_WHEELCHAIR = "do you have handicaps parking near your business?"
-B_OWNER_QUESTION_CINEMA4_VISION = "in case your door is transparent, do you have a colored sticker?(if your door isn't transparent press 'yes')"
+B_OWNER_QUESTION_CINEMA4_VISION = "in case your door is transparent, do you have a colored sticker?\n(if your door isn't transparent press 'yes')"
 B_OWNER_QUESTION_CINEMA5_WHEELCHAIR = "do you have special seats for handicaps?"
-B_OWNER_QUESTION_CINEMA6_WHEELCHAIR = "in case you have stairs in the entrance,do you have a ramp?(if you don't have stairs press 'yes')"
+B_OWNER_QUESTION_CINEMA6_WHEELCHAIR = "in case you have stairs in the entrance,do you have a ramp?\n(if you don't have stairs press 'yes')"
 B_OWNER_QUESTION_CINEMA7_HEARING = "do you have a special sound system for hearing impaired?"
 B_OWNER_QUESTION_CINEMA8_WHEELCHAIR = "do you have bathroom stalls for handicaps?"
 B_OWNER_QUESTION_CINEMA9_WHEELCHAIR = "does your business's corridor width equal or bigger than 130 cm?"
@@ -22,10 +22,10 @@ B_OWNER_QUESTION_CINEMA9_WHEELCHAIR = "does your business's corridor width equal
 
 
 
-B_OWNER_QUESTION_RESTAURANT1_WHEELCHAIR = "in case you have more than 1 floor,does your business have an elevator?(if you have 1 floor press 'yes')"
+B_OWNER_QUESTION_RESTAURANT1_WHEELCHAIR = "in case you have more than 1 floor,does your business have an elevator?\n(if you have 1 floor press 'yes')"
 B_OWNER_QUESTION_RESTAURANT2_WHEELCHAIR = "does your door width equal or bigger than 80 cm?"
-B_OWNER_QUESTION_RESTAURANT3_VISION = "in case your door is transparent, do you have a colored sticker?(if your door isn't transparent press 'yes')"
-B_OWNER_QUESTION_RESTAURANT4_WHEELCHAIR = "in case you have stairs in the entrance,do you have a ramp?(if you don't have stairs press 'yes')"
+B_OWNER_QUESTION_RESTAURANT3_VISION = "in case your door is transparent, do you have a colored sticker?\n(if your door isn't transparent press 'yes')"
+B_OWNER_QUESTION_RESTAURANT4_WHEELCHAIR = "in case you have stairs in the entrance,do you have a ramp?\n(if you don't have stairs press 'yes')"
 B_OWNER_QUESTION_RESTAURANT5_WHEELCHAIR = "do you have bathroom stalls for handicaps?"
 B_OWNER_QUESTION_RESTAURANT6_WHEELCHAIR = "do you have handicaps parking near your business?"
 B_OWNER_QUESTION_RESTAURANT7_WHEELCHAIR = "does your business's corridor width equal or bigger than 130 cm?"
--- Gui.py
+++ Gui.py
@@ -1,11 +1,17 @@
 from tkinter import *
 from tkinter import ttk
-from DataAccess import *
-from Const import *
+from hackathon.DataAccess import *
+from hackathon.Const import *
 
 screen = Tk()
 screen.title(TITLE)
 screen.geometry(f"{WIDTH}x{HEIGHT}")
+details_business = {
+    "name": "",
+    "area": "",
+    "address": "",
+    "category": ""
+}
 
 
 def center(scr=screen):
@@ -26,32 +32,123 @@ def center(scr=screen):
     scr.deiconify()
 
 
+def restaurant_pressed1():
+    Button(screen_owner, bg="dark blue", fg="white", text="Restaurant", font="calibri 15", height=1,
+           width=15, command=restaurant_pressed2).place(x=40, y=400)
+    details_business["category"] = "restaurant"
+
+
+def restaurant_pressed2():
+    Button(screen_owner, bg="light grey", fg="black", text="Restaurant", font="calibri 15", height=1,
+           width=15, command=restaurant_pressed1).place(x=40, y=400)
+
+
+def cinema_pressed1():
+    Button(screen_owner, bg="dark blue", fg="white", text="Cinema", font="calibri 15", height=1,
+           width=15, command=cinema_pressed2).place(x=230, y=400)
+    details_business["category"] = "cinema"
+
+
+def cinema_pressed2():
+    Button(screen_owner, bg="light grey", fg="black", text="Cinema", font="calibri 15", height=1,
+           width=15, command=cinema_pressed1).place(x=230, y=400)
+
+
+def store_pressed1():
+    Button(screen_owner, bg="dark blue", fg="white", text="Store", font="calibri 15", height=1,
+           width=15, command=store_pressed2).place(x=410, y=400)
+    details_business["category"] = "store"
+
+
+def store_pressed2():
+    Button(screen_owner, bg="light grey", fg="black", text="Store", font="calibri 15", height=1,
+           width=15, command=store_pressed1).place(x=410, y=400)
+
+
 def owner():
-    global screen1
-    screen1 = Toplevel(screen)
-    screen1.title("Business Owner")
-    screen1.geometry(f"{WIDTH}x{HEIGHT}")
-    center(screen1)
-    screen1.configure(bg='light blue')
+    global screen_owner
+    global name_business
+    global category
+    global address
+    global variable
+    screen_owner = Toplevel(screen)
+    screen_owner.title("Business Owner")
+    screen_owner.geometry(f"{WIDTH}x{HEIGHT}")
+    center(screen_owner)
+    screen_owner.configure(bg='light blue')
+    question = Label(screen_owner, text="Hello, Business Owner", bg="light blue", font=("arial italic", 18)).pack()
+
+    name_business = StringVar()
+    category = StringVar()
+    address = StringVar()
+
+    Label(screen_owner, text="Please enter details bellow: ", bg='light blue', font="calibri 15 bold").pack()
+    Label(screen_owner, text="Name of your business *", bg='light blue', font="calibri 15").pack()
+    name_entry = Entry(screen_owner, textvariable=name_business).place(width=200, height=30, x=190, y=100)
+
+    Label(screen_owner, text="What category does your business refers to? *", bg='light blue', font="calibri 15").place(
+        x=130, y=360)
+    restaurant_btn = Button(screen_owner, bg="light grey", fg="black", text="Restaurant", font="calibri 15", height=1,
+                            width=15, command=restaurant_pressed1)
+    restaurant_btn.place(x=40, y=400)
+    cinema_btn = Button(screen_owner, bg="light grey", fg="black", text="Cinema", font="calibri 15", height=1,
+                        width=15, command=cinema_pressed1)
+    cinema_btn.place(x=230, y=400)
+    store_btn = Button(screen_owner, bg="light grey", fg="black", text="Store", font="calibri 15", height=1,
+                       width=15, command=store_pressed1)
+    store_btn.place(x=410, y=400)
+
+    Label(screen_owner, text="The area of your business *", bg='light blue', font="calibri 15").place(
+        x=180, y=160)
+    variable = StringVar(screen_owner)
+    variable.set("select option")  # default value
+    options = OptionMenu(screen_owner, variable, *OPTIONS_AREA)
+    options.place(x=230, y=200, width=120, height=30)
+
+    Label(screen_owner, text="The address of your business *", bg='light blue', font="calibri 15").place(x=170, y=260)
+    address_entry = Entry(screen_owner, textvariable=address).place(x=190, y=300, width=200, height=30)
+
+    done_btn = Button(screen_owner, bg="light grey", fg="dark blue", text="Submit", font="calibri 15", height=1,
+                      width=60, command=owner_done).place(x=0, y=460)
+
+
+def owner_done():
+    details_business["name"] = name_business.get()
+    details_business["area"] = variable.get()
+    details_business["address"] = address.get()
+    print(details_business)
 
 
 def customer():
-    print("customer")
+    global screen_customer
+    screen_customer = Toplevel(screen)
+    screen_customer.title("Customer")
+    screen_customer.geometry(f"{WIDTH}x{HEIGHT}")
+    center(screen_customer)
+    screen_customer.configure(bg='light blue')
+    question = Label(screen_customer, text="Hello Customer", bg="light blue", font=("arial italic", 18)).pack()
 
 
-# Add a canvas widget
-center()
-canvas = Canvas(screen, width=WIDTH, height=HEIGHT, bg="light blue")
+def main_start():
+    # Add a canvas widget
+    center()
+    canvas = Canvas(screen, width=WIDTH, height=HEIGHT, bg="light blue")
 
-img = PhotoImage(file=START_IMAGE, height=HEIGHT, width=WIDTH)
-canvas.background = img
-canvas.create_image(0, 0, anchor=NW, image=img)  # background image
-screen.configure(bg='light blue')
+    img = PhotoImage(file=START_IMAGE, height=HEIGHT, width=WIDTH)
+    canvas.background = img
+    canvas.create_image(0, 0, anchor=NW, image=img)  # background image
+    screen.configure(bg='light blue')
 
-# Create a button in canvas widget
-business_owner_button = Button(screen, bg="dark blue", fg="white", text="Business Owner", font="calibri 17", height=2, width=20, command=owner).pack()
+    # Create a button in canvas widget
+    business_owner_button = Button(screen, bg="dark blue", fg="white", text="Business Owner", font="calibri 17", height=2,
+                                   width=20, command=owner).pack()
 
-customer_butto = Button(screen, bg="dark blue", fg="white", text="Customer", font="calibri 17", height=2, width=20, command=customer).pack(pady=10)
-canvas.pack()
+    customer_butto = Button(screen, bg="dark blue", fg="white", text="Customer", font="calibri 17", height=2, width=20,
+                            command=customer).pack(pady=10)
+    canvas.pack()
 
-screen.mainloop()
+    screen.mainloop()
+
+
+if __name__ == '__main__':
+    main_start()
